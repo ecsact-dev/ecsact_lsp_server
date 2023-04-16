68b52f8b3fe112cb55f370c7a590b6b9ebf7604f
Ezekiel Warren <zekewarren@gmail.com>
2023-04-15 22:04:42 -0700

    feat: interpret ecsact files for better errors

--- details/workspace_manager.hh
+++ details/workspace_manager.hh
@@ -2,11 +2,14 @@
 
 #include <unordered_map>
 #include <unordered_set>
+#include <set>
 #include <string>
 #include <vector>
 #include <string_view>
+#include <ranges>
 #include "ecsact/parse.h"
 #include "ecsact/interpret/eval.h"
+#include "ecsact/runtime/dynamic.h"
 #include "magic_enum.hpp"
 
 #include "./messages.hh"
@@ -104,17 +107,6 @@ class workspace_manager {
 			doc.next_parse_view = parse_stack(doc.next_parse_view, status, stack);
 		}
 
-		if(!doc.parse_stacks.empty()) {
-			auto& first_statement = doc.parse_stacks.front().back();
-			if(first_statement.type != ECSACT_STATEMENT_PACKAGE) {
-				diagnostics.push_back(diagnostic{
-					.range{},
-					.severity = diagnostic_severity::warning,
-					.message = "First statement must be a package statement",
-				});
-			}
-		}
-
 		for(auto& status : doc.parse_statuses) {
 			if(ecsact_is_error_parse_status_code(status.code)) {
 				auto r = get_source_range(doc.full_text, status.error_location);
@@ -137,6 +129,91 @@ class workspace_manager {
 		);
 	}
 
+	auto _interpret_document(std::string uri, int version, document_state& doc) {
+		using std::views::drop;
+		using namespace std::string_literals;
+
+		if(doc.parse_stacks.empty()) {
+			return;
+		}
+
+		if(doc.package_id) {
+			ecsact_destroy_package(*doc.package_id);
+			doc.package_id = std::nullopt;
+		}
+
+		auto  diagnostics = std::vector<diagnostic>{};
+		auto& package_parse_stack = doc.parse_stacks.front();
+
+		if(package_parse_stack.empty()) {
+			return;
+		}
+
+		auto& package_statement = package_parse_stack.front();
+
+		if(package_statement.type != ECSACT_STATEMENT_PACKAGE) {
+			diagnostics.push_back(diagnostic{
+				.range{},
+				.severity = diagnostic_severity::warning,
+				.message = "First statement must be a package statement",
+			});
+		} else {
+			doc.package_id = ecsact_eval_package_statement( //
+				&package_statement.data.package_statement
+			);
+		}
+
+		if(doc.package_id) {
+			auto eval_statement_tracker = std::set<int32_t>{};
+
+			// Evaluate all statements in stacks except first (package)
+			for(auto& parse_stack : doc.parse_stacks | drop(1)) {
+				for(auto idx = 0; parse_stack.size() > idx; ++idx) {
+					auto& statement = parse_stack[idx];
+					if(eval_statement_tracker.contains(statement.id)) {
+						continue;
+					}
+					eval_statement_tracker.insert(statement.id);
+
+					auto eval_err = ecsact_eval_statement(
+						*doc.package_id,
+						static_cast<int32_t>(parse_stack.size()),
+						parse_stack.data() + idx
+					);
+
+					if(eval_err.code != ECSACT_EVAL_OK) {
+						auto r = get_source_range(doc.full_text, eval_err.relevant_content);
+						auto message = std::string{magic_enum::enum_name(eval_err.code)};
+						if(eval_err.relevant_content.length > 0) {
+							message += " ";
+							message += std::string_view{
+								eval_err.relevant_content.data,
+								static_cast<size_t>(eval_err.relevant_content.length),
+							};
+						}
+
+						diagnostics.push_back(diagnostic{
+							.range = r,
+							.severity = diagnostic_severity::error,
+							.message = message,
+						});
+					}
+				}
+			}
+		}
+
+		if(!diagnostics.empty()) {
+			send.send_notification(
+				"textDocument/publishDiagnostics",
+				nlohmann::json{
+					{"uri", uri},
+					{"version", version},
+					{"diagnostics", diagnostics},
+				}
+			);
+		}
+	}
+
 public:
 	workspace_manager(Send&& send) : send(std::forward<Send>(send)) {
 	}
@@ -155,6 +232,7 @@ public:
 		doc.full_text = std::move(text);
 		doc.next_parse_view = doc.full_text;
 		_parse_document(uri, initial_version, doc);
+		_interpret_document(uri, initial_version, doc);
 	}
 
 	auto update_document(std::string uri, int version, std::string text) -> void {
@@ -162,6 +240,7 @@ public:
 		doc.full_text = std::move(text);
 		doc.next_parse_view = doc.full_text;
 		_parse_document(uri, version, doc);
+		_interpret_document(uri, version, doc);
 	}
 
 	auto remove_document(std::string uri) -> void {
--- test/nvim_test.lua
+++ test/nvim_test.lua
@@ -1,6 +1,3 @@
--- File for testing in neovim
--- :luafile %
-
 vim.lsp.set_log_level("TRACE")
 
 local cmd = vim.fn.getcwd() .. "/bazel-bin/ecsact_lsp_server"
@@ -9,12 +6,27 @@ if vim.fn.has('win32') then
 	cmd = cmd .. ".exe"
 end
 
-require("lspconfig").ecsact.setup {
-	root_dir = function()
-		return vim.fn.getcwd()
-	end,
-	cmd = {cmd, "--stdio"},
-}
+local diagnostics_examples = vim.split(
+	vim.fn.glob(vim.fn.getcwd() .. '/test/diagnostics_examples/*.ecsact'),
+	'\n'
+)
 
-vim.cmd [[:LspStop ecsact]]
+local view_time = 5000
+
+for i, file in ipairs(diagnostics_examples) do
+	vim.defer_fn(function()
+		vim.cmd('edit ' .. file)
+		vim.bo.filetype = "ecsact"
+
+		vim.lsp.start({
+			name = 'ecsact',
+			cmd = {cmd, "--stdio"},
+			root_dir = vim.fn.getcwd(),
+		})
+	end, view_time * (i - 1))
+end
+
+vim.defer_fn(function()
+	vim.cmd('qa!')
+end, view_time * (#diagnostics_examples))
 
