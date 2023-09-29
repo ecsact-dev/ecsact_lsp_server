42ceb92d427a2993f966f626a1286a4c3f8f14dd
Ezekiel Warren <zekewarren@gmail.com>
2023-09-29 16:18:43 -0700

    chore: formatting

--- details/interfaces.hh
+++ details/interfaces.hh
@@ -12,44 +12,37 @@
 namespace ecsact_lsp {
 
 template<typename Send>
-concept send_interface =
-	requires(Send& send) {
-		{
-			send.send_request(
-				std::declval<std::string>(),
-				std::declval<nlohmann::json>(),
-				std::declval<std::function<void(nlohmann::json)>>()
-			)
-		};
-
-		{
-			send.send_notification(
-				std::declval<std::string>(),
-				std::declval<nlohmann::json>()
-			)
-		};
-
-		{
-			send.show_message(
-				std::declval<message_type>(),
-				std::declval<std::string>(),
-				std::declval<std::vector<message_action_item>>(),
-				std::declval<std::function<void(std::optional<message_action_item>)>>()
-			)
-		};
-
-		{
-			send.show_message(
-				std::declval<message_type>(),
-				std::declval<std::string>()
-			)
-		};
-
-		{
-			send.log_message(
-				std::declval<message_type>(),
-				std::declval<std::string>()
-			)
-		};
+concept send_interface = requires(Send& send) {
+	{
+		send.send_request(
+			std::declval<std::string>(),
+			std::declval<nlohmann::json>(),
+			std::declval<std::function<void(nlohmann::json)>>()
+		)
 	};
+
+	{
+		send.send_notification(
+			std::declval<std::string>(),
+			std::declval<nlohmann::json>()
+		)
+	};
+
+	{
+		send.show_message(
+			std::declval<message_type>(),
+			std::declval<std::string>(),
+			std::declval<std::vector<message_action_item>>(),
+			std::declval<std::function<void(std::optional<message_action_item>)>>()
+		)
+	};
+
+	{
+		send.show_message(std::declval<message_type>(), std::declval<std::string>())
+	};
+
+	{
+		send.log_message(std::declval<message_type>(), std::declval<std::string>())
+	};
+};
 }
